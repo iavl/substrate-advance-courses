0c22af4e041d547c8bc8e1f097913c9e27e8c8eb
iavl <z6521256@gmail.com>
2021-01-30 20:38:46 +0800

    add erc20

--- course5/erc20/lib.rs
+++ course5/erc20/lib.rs
@@ -5,36 +5,98 @@ use ink_lang as ink;
 #[ink::contract]
 mod erc20 {
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
+    use ink_storage::collections::HashMap as StorageHashMap;
+
     #[ink(storage)]
     pub struct Erc20 {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+        total_supply: Balance,
+        balances: StorageHashMap<AccountId, Balance>,
+        allowance: StorageHashMap<(AccountId, AccountId), Balance>
     }
+
+    #[ink(event)]
+    pub struct Transfer {
+        #[ink(topic)]
+        from: AccountId,
+        #[ink(topic)]
+        to: AccountId,
+        value: Balance,
+    }
+
+    #[derive(Debug, PartialEq, Eq, scale::Encode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        InsufficientBalance,
+    }
+
+    pub type Result<T> = core::result::Result<T, Error>;
 
     impl Erc20 {
         /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new(total_supply: Balance) -> Self {
+            let caller = Self::env().caller();
+            let mut balances = StorageHashMap::new();
+            balances.insert(caller, total_supply);
+            let instance = Self {
+                total_supply: total_supply,
+                balances: balances,
+                allowances: StorageHashMap::new(),
+            };
+            
+            instance
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
+        pub fn total_supply(&self) -> Balance {
+            *self.total_supply
+        }
+
+        #[ink(message)]
+        pub fn balance_of(&self, owner: AccountId) -> Balance {
+            *self.balances.get(&owner).unwrap_or(&0)
+        }
+
+        // #[ink(message)]
+        // pub fn allowance(&self, owner: AccountId, spender: AccountId) -> Balance {
+        // }
+
+        #[ink(message)]
+        pub fn transfer(&mut self, to: AccountId, value: Balance) -> Result<()> {
+            let who = Self::env().caller();
+            self.transfer_helper(who, to, value);
+        }
+
+        // #[ink(message)]
+        // pub fn transferFrom(&mut self, from: AccountId,  to: AccountId, value: Balance) -> Result<()> {
+        // }
+
+
+        // #[ink(message)]
+        // pub fn burn() {
+        // }
+
+        // #[ink(message)]
+        // pub fn issue(&mut self) -> Result<()> {
+        // }
+
+        fn transfer_helper(&mut self, from: AccountId, to: AccountId, value: Balance) -> Result<()> {
+            let from_balance = self.balance_of(from);
+            if from_balance < value {
+                return Err(Error::InsufficientBalance);
+            }
+
+            self.balances.insert(from, from_balance - value);
+            let to_balance = self.balance_of(to);
+            self.balances.insert(to, to_balance + value);
+
+            self.env().emit_event(Transfer {
+                from: from,
+                to: to,
+                value: value,
+            });
+
+            Ok(())
         }
 
         /// Simply returns the current value of our `bool`.
@@ -54,18 +116,10 @@ mod erc20 {
 
         /// We test if the default constructor does its job.
         #[test]
-        fn default_works() {
-            let erc20 = Erc20::default();
-            assert_eq!(erc20.get(), false);
-        }
+        fn create_contract_works() {
+            let erc20 = Erc20::new(1000);
 
-        /// We test a simple use case of our contract.
-        #[test]
-        fn it_works() {
-            let mut erc20 = Erc20::new(false);
-            assert_eq!(erc20.get(), false);
-            erc20.flip();
-            assert_eq!(erc20.get(), true);
+            assert_eq!(erc20.total_supply(), 1000);
         }
     }
 }
