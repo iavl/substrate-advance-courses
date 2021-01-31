f3d1428f23838810904c403647107b921663c671
iavl <z6521256@gmail.com>
2021-01-31 12:02:37 +0800

    add unit test

--- course5/erc20/lib.rs
+++ course5/erc20/lib.rs
@@ -9,40 +9,69 @@ mod erc20 {
 
     #[ink(storage)]
     pub struct Erc20 {
+        /// Total token supply
         total_supply: Balance,
+        /// Mapping owner -> balance
         balances: StorageHashMap<AccountId, Balance>,
-        allowance: StorageHashMap<(AccountId, AccountId), Balance>
+        allowances: StorageHashMap<(AccountId, AccountId), Balance>,
     }
 
     #[ink(event)]
     pub struct Transfer {
         #[ink(topic)]
-        from: AccountId,
+        from: Option<AccountId>,
         #[ink(topic)]
-        to: AccountId,
+        to:Option<AccountId>,
         value: Balance,
     }
 
+    #[ink(event)]
+    pub struct Approval {
+        #[ink(topic)]
+        owner: AccountId,
+        #[ink(topic)]
+        spender: AccountId,
+        value: Balance,
+    }
+
+    #[ink(event)]
+    pub struct Burn {
+        #[ink(topic)]
+        owner: AccountId,
+        value: Balance,
+    }
+
+    /// Error types
     #[derive(Debug, PartialEq, Eq, scale::Encode)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
     pub enum Error {
         InsufficientBalance,
+        InsufficientBurn,
     }
 
+    // Result type
     pub type Result<T> = core::result::Result<T, Error>;
 
     impl Erc20 {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
+        /// Creates a new ERC20 contract.
         #[ink(constructor)]
-        pub fn new(total_supply: Balance) -> Self {
+        pub fn new(init_supply: Balance) -> Self {
             let caller = Self::env().caller();
             let mut balances = StorageHashMap::new();
-            balances.insert(caller, total_supply);
+            balances.insert(caller, init_supply);
+
             let instance = Self {
-                total_supply: total_supply,
-                balances: balances,
-                allowance: StorageHashMap::new(),
+                total_supply: init_supply,
+                balances,
+                allowances: StorageHashMap::new(),
             };
+
+            // emit Transfer event
+            Self::env().emit_event(Transfer {
+                from: None,
+                to: Some(caller),
+                value: init_supply,
+            });
             
             instance
         }
@@ -54,30 +83,62 @@ mod erc20 {
 
         #[ink(message)]
         pub fn balance_of(&self, owner: AccountId) -> Balance {
-            *self.balances.get(&owner).unwrap_or(&0)
+            self.balances.get(&owner).copied().unwrap_or(0)
         }
 
-        // #[ink(message)]
-        // pub fn allowance(&self, owner: AccountId, spender: AccountId) -> Balance {
-        // }
+        #[ink(message)]
+        pub fn allowance(&self, owner: AccountId, spender: AccountId) -> Balance {
+            self.allowances.get(&(owner, spender)).copied().unwrap_or(0)
+        }
 
         #[ink(message)]
         pub fn transfer(&mut self, to: AccountId, value: Balance) -> Result<()> {
-            let who = Self::env().caller();
+            let who = self.env().caller();
             self.transfer_helper(who, to, value)
         }
 
-        // #[ink(message)]
-        // pub fn transferFrom(&mut self, from: AccountId,  to: AccountId, value: Balance) -> Result<()> {
-        // }
+        #[ink(message)]
+        pub fn transfer_from(&mut self, from: AccountId,  to: AccountId, value: Balance) -> Result<()> {
+            let caller = self.env().caller();
+            let allowance = self.allowance(from, caller);
+            if allowance < value {
+                return Err(Error::InsufficientBalance);
+            }
 
+            self.allowances.insert((from, to), allowance - value);
+            self.transfer_helper(from, to, value)
+        }
+
+        #[ink(message)]
+        pub fn approve(&mut self, spender: AccountId, value: Balance) -> Result<()> {
+            let owner = self.env().caller();
+            self.allowances.insert((owner, spender), value);
+
+            self.env().emit_event(Approval {
+                owner,
+                spender,
+                value,
+            });
+
+            Ok(())
+        }
+
+        #[ink(message)]
+        pub fn burn(&mut self, value: Balance) -> Result<()> {
+            let owner = self.env().caller();
+            let balance = self.balance_of(owner);
+            if balance < value {
+                return Err(Error::InsufficientBurn);
+            }
+
+            self.balances.insert(owner, balance - value);
+            self.total_supply = self.total_supply - value;
+
+            Ok(())
+        }
 
         // #[ink(message)]
-        // pub fn burn() {
-        // }
-
-        // #[ink(message)]
-        // pub fn issue(&mut self) -> Result<()> {
+        // pub fn issue(&mut self, to: AccountId, value: Balance) -> Result<()> {
         // }
 
         fn transfer_helper(&mut self, from: AccountId, to: AccountId, value: Balance) -> Result<()> {
@@ -91,14 +152,13 @@ mod erc20 {
             self.balances.insert(to, to_balance + value);
 
             self.env().emit_event(Transfer {
-                from: from,
-                to: to,
+                from: Some(from),
+                to: Some(to),
                 value: value,
             });
 
             Ok(())
         }
-
     }
 
     /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
@@ -108,13 +168,59 @@ mod erc20 {
     mod tests {
         /// Imports all the definitions from the outer scope so we can use them here.
         use super::*;
+        use ink_lang as ink;
 
         /// We test if the default constructor does its job.
-        #[test]
+        #[ink::test]
         fn create_contract_works() {
+            // Constructor works.
             let erc20 = Erc20::new(1000);
 
             assert_eq!(erc20.total_supply(), 1000);
+        }
+
+        #[ink::test]
+        fn balance_of_works() {
+            let erc20 = Erc20::new(1000);
+
+            let accounts = 
+                ink_env::test::default_accounts::<ink_env::DefaultEnvironment>()
+                .expect("Cannot get accounts");
+
+            assert_eq!(erc20.balance_of(accounts.alice), 1000);
+            assert_eq!(erc20.balance_of(accounts.bob), 0);
+        }
+
+        #[ink::test]
+        fn transfer_works() {
+            // Constructor works.
+            let mut erc20 = Erc20::new(1000);
+            // Transfer event triggered during initial construction.
+            let accounts =
+                ink_env::test::default_accounts::<ink_env::DefaultEnvironment>()
+                    .expect("Cannot get accounts");
+
+            assert_eq!(erc20.balance_of(accounts.bob), 0);
+            // Alice transfers 10 tokens to Bob.
+            assert_eq!(erc20.transfer(accounts.bob, 10), Ok(()));
+            // Bob owns 10 tokens.
+            assert_eq!(erc20.balance_of(accounts.bob), 10);
+
+            let emitted_events = ink_env::test::recorded_events().collect::<Vec<_>>();
+            assert_eq!(emitted_events.len(), 2);
+        }
+
+        #[ink::test]
+        fn burn_works() {
+            // Constructor works.
+            let mut erc20 = Erc20::new(100);
+            // Transfer event triggered during initial construction.
+            let accounts =
+                ink_env::test::default_accounts::<ink_env::DefaultEnvironment>()
+                    .expect("Cannot get accounts");
+
+            assert_eq!(erc20.brun(accounts.alice, 10), Ok(()));
+            assert_eq!(erc20.balance_of(accounts.alice), 90);
         }
     }
 }
